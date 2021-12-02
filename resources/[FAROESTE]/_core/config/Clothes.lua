147ec4785db15c4c0e641619a8620325ec5eed75
redharlow2614 <71796038+redharlow2614@users.noreply.github.com>
2021-12-01 17:50:20 -0700

    translated to english

--- resources/[FAROESTE]/_core/config/Clothes.lua
+++ resources/[FAROESTE]/_core/config/Clothes.lua
@@ -1,42 +1,42 @@
 Clothes = {
     ["chapeu"] = {
-        name = "chapeu",
+        name = "hat",
         hash = 0x9925C067
     },
     ["camisa"] = {
-        name = "camisa",
+        name = "shirt",
         hash = 0x2026C46D
     },
     ["colete"] = {
-        name = "colete",
+        name = "vest",
         hash = 0x485EE834
     },
     ["casaco"] = {
-        name = "casaco",
+        name = "coat",
         hash = 0x662AC34
     },
     ["calca"] = {
-        name = "calca",
+        name = "pants",
         hash = 0x1D4C528A
     },
     ["mascara"] = {
-        name = "mascara",
+        name = "mask",
         hash = 0x7505EF42
     },
     ["botas"] = {
-        name = "botas",
+        name = "boots",
         hash =0x777EC6EF
     },
     ["luvas"] = {
-        name = "luvas",
+        name = "gloves",
         hash = 0xEABE0032
     },
     ["saia"] = {
-        name = "saia",
+        name = "dresses",
         hash = 0xA0E3AB7F
     },
     ["coldre"] = {
-        name = "coldre",
+        name = "holsters",
         hash = 0x9B2C8B89
     }
 }
