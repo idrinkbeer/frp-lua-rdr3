b42ae63ade7052c9d1c9e6d29e207479c9ea906f
redharlow2614 <71796038+redharlow2614@users.noreply.github.com>
2021-12-01 23:20:32 -0700

    translated

--- resources/[FAROESTE]/[SYSTEM]/frp_tents/config/Tents.lua
+++ resources/[FAROESTE]/[SYSTEM]/frp_tents/config/Tents.lua
@@ -4,80 +4,80 @@ Config.Tents = {
     {
         id = 1,
         tentModel = 615627222,
-        tentText = "Tenda",
-        tentLabel = "Armando Tenda...",
+        tentText = "Tent",
+        tentLabel = "Setting up Tent...",
         tentSpawnTime = 15000,
         itemId = "tents_small"
     },
     {
         id = 2,
         tentModel = 1231676579,
-        tentText = "Tenda",
-        tentLabel = "Armando Tenda...",
+        tentText = "Tent",
+        tentLabel = "Setting up Tent...",
         tentSpawnTime = 25000,
         itemId = "tents_med"
     },
     {
         id = 3,
         tentModel = -838945132,
-        tentText = "Tenda",
-        tentLabel = "Armando Tenda...",
+        tentText = "Tent",
+        tentLabel = "Setting up Tent...",
         tentSpawnTime = 30000,
         itemId = "tents_big"
     },
     {
         id = 5,
         tentModel = 1946434792,
-        tentText = "Fogueira",
-        tentLabel = "Armando Fogueira...",
+        tentText = "Bonfire",
+        tentLabel = "Setting up Bonfire...",
         tentSpawnTime = 10000,
         itemId = "acamp_firesmall"
     }, 
     {
         id = 6,
         tentModel = 634946146,
-        tentText = "Fogueira",
-        tentLabel = "Armando Fogueira...",
+        tentText = "Bonfire",
+        tentLabel = "Setting up Bonfire...",
         tentSpawnTime = 10000,
         itemId = "acamp_firemed"
     }, 
     {
         id = 7,
         tentModel = 634946146,
-        tentText = "Fogueira",
-        tentLabel = "Armando Fogueira...",
+        tentText = "Bonfire",
+        tentLabel = "Setting up Bonfire...",
         tentSpawnTime = 10000,
         itemId = "acamp_firebig"
     }, 
     {
         id = 8,
         tentModel = -689630872,
-        tentText = "Fogueira",
-        tentLabel = "Armando Fogueira...",
+        tentText = "Bonfire",
+        tentLabel = "Setting up Bonfire...",
         tentSpawnTime = 10000,
         itemId = "acamp_firesmal_pan"
     }, 
     {
         id = 9,
         tentModel = 737690157,
-        tentText = "Saco de Dormir",
-        tentLabel = "Armando Saco de Dormir...",
+        tentText = "Sleeping bag",
+        tentLabel = "Rolling out sleeping bag...",
         tentSpawnTime = 5000,
         itemId = "acamp_bedroll"
     }, 
     {
         id = 10,
         tentModel = -1223489959,
-        tentText = "Tenda",
-        tentLabel = "Armando Tenda...",
+        tentText = "Tent",
+        tentLabel = "Setting up Tent...",
         tentSpawnTime = 30000,
         itemId = "acamp_tents_med_open"
     },  
     {
         id = 11,
         tentModel = -1775023743,
-        tentText = "Tenda",
-        tentLabel = "Armando Tenda...",
+        tentText = "Tent",
+        tentLabel = "Setting up Tent...",
         tentSpawnTime = 30000,
         itemId = "acamp_tents_med_closed"
     },      
